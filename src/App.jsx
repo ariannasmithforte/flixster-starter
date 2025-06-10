79ed2059b10520562651d27a71148bbdaead80a9
Arianna Smith-Forte <ariannasforte@meta.com>
2025-06-09 20:35:34 -0700

    first chnages: created header and movie jsx for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import Header from './Header.jsx'
+import MovieCard from './Movie.jsx'
+import data from './data/data.js'
 
 const App = () => {
   return (
-    <div className="App">
-    
+    <>
+    <Header />
+    <div className="movies-container">
+      {data.results.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie_image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          movie_name={movie.title}
+          movie_rating={movie.vote_average}
+        />
+      ))}
     </div>
-  )
+    </>
+)
 }
 
 export default App
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+        <h1 className = "App-header">Flixter</h1>
+        </>
+    )
+}
+
+export default Header;
